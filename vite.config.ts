7d743324ed1dbce74241779ecc111a3a3bfd47db
Hao Jin <haojinhj@gmail.com>
2026-02-17 11:59:36 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => {
         },
       },
       plugins: [react()],
+      base: "/six-poker/",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
