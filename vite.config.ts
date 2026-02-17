1b85d0a1129cff6c9088485aca16be5924ff29fb
Hao Jin <haojinhj@gmail.com>
2026-02-17 12:02:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
         proxy: {
-          '/api': {
+          '/six-poker/api': {
             target: 'http://47.93.33.214:5008',
             changeOrigin: true,
           },
